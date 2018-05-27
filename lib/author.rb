056f7354eca334061ebeb6dfb4db5ef735d20e6b
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2018-05-27 23:27:41 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -11,13 +11,18 @@ class Author
   def add_post(post)
     post.author = self
     @posts << post
+    @@ALL_POSTS << post
   end
 
   def add_post_by_title(title)
     new_post = Post.new(title)
     new_post.author = self
     @posts << new_post
+    @@ALL_POSTS << new_post
   end
 
+  def self.post_count
+    @@ALL_POSTS.count
+  end
 
 end
